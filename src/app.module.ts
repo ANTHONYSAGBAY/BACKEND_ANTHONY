25d891d95549cf2e2acd7e150d978002797be853
ANTHONYSAGBAY <amsagbay@sudamericano.edu.ec>
2025-06-06 00:22:56 -0500

    Primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsModule } from './notifications/notifications.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', // tu usuario de postgres
+      password: '0107101503',    // tu contraseña
+      database: 'notification_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    NotificationsModule,
+  ],
+})
+export class AppModule {}
+
